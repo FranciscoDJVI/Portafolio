358d1f38e99a41bf10de9d1e607eb6db049c929f
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-09-24 22:27:14 -0500

    add component card and styling

--- portafolio/src/components/Card.jsx
+++ portafolio/src/components/Card.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const styleCard = {
+  textAlign: "justify",
+  fontSize: "2rem",
+  border: "5px #dddddd solid",
+  borderRadius: "15px",
+  padding: "10px",
+};
 
 function Card(props) {
   return (
-    <div>
+    <motion.div
+      style={styleCard}
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
       <h2>{props.title}</h2>
       <p>{props.textInfo}</p>
-    </div>
+    </motion.div>
   );
 }
 
--- portafolio/src/components/PresentationText.jsx
+++ portafolio/src/components/PresentationText.jsx
@@ -1,25 +1,13 @@
 import { motion } from "framer-motion";
 import Card from "./Card";
 
-const customP = {
-  fontSize: "2rem",
-  textAlign: "justify",
-};
-
 function PresentationText() {
   const titleInfo = "Presentacion";
   const text =
     "Hola, mucho gusto soy ingeniero de sistemas egresado de la Univerisidad del Pacífico en la ciudad del Buenaventura, Valle del cauca-Colombia. soy un desarrollador Jr, apasionado por el desarrollo y con intensiones de ser un excelente desarrollador cada día.";
   return (
     <div className="presentation-text">
-      <Card
-        style={customP}
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: "easeOut" }}
-        title={titleInfo}
-        textInfo={text}
-      />
+      <Card title={titleInfo} textInfo={text} />
     </div>
   );
 }

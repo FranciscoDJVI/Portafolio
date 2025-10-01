d08fdd342e9929202efe45bc534f8ec8edf2ccba
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-09-30 19:54:49 -0500

    fix: add to the Route

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Avatar from "./components/Avatar";
 import PresentationText from "./components/PresentationText";
 import Hobbies from "./components/Hobbies";
+import Estudies from "./components/Estudies.jsx";
 import Skills from "./components/Skills";
 import ProjectPersonal from "./components/Projects";
 import { Routes, Route } from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
       <Route path="/avatar" element={<Avatar />} />
       <Route path="/presentation" element={<PresentationText />} />
       <Route path="/hobbies" element={<Hobbies />} />
+      <Route path="/estudies" element={<Estudies />} />
       <Route path="/skills" element={<Skills />} />
       <Route path="/projects" element={<ProjectPersonal />} />
     </Routes>

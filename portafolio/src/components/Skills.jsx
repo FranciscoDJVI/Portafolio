8b954b556c6d3d4edc91ad470eb565beb6e01965
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-04 18:02:12 -0500

    fix: add card components

--- portafolio/src/components/Skills.jsx
+++ portafolio/src/components/Skills.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -9,29 +8,33 @@ import {
   faJs,
 } from "@fortawesome/free-brands-svg-icons";
 
+import Card from "./Card";
+
 function Skills() {
   return (
-    <div className="skills">
-      <motion.h2
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: "easeOut" }}
-      >
-        Skills
-      </motion.h2>
-      <motion.aside
-        className="icons"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: "easeOut" }}
-      >
-        <FontAwesomeIcon icon={faPython} />
-        <FontAwesomeIcon icon={faReact} />
-        <FontAwesomeIcon icon={faGit} />
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faJs} />
-      </motion.aside>
-    </div>
+    <Card>
+      <div className="skills">
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          Skills
+        </motion.h2>
+        <motion.aside
+          className="icons"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <FontAwesomeIcon icon={faPython} />
+          <FontAwesomeIcon icon={faReact} />
+          <FontAwesomeIcon icon={faGit} />
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faJs} />
+        </motion.aside>
+      </div>
+    </Card>
   );
 }
 

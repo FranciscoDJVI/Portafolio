804fa30714ffd2c3b1e699899159d427f796b14a
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-04 18:06:22 -0500

    fix: delete card component

--- portafolio/src/components/Skills.jsx
+++ portafolio/src/components/Skills.jsx
@@ -12,29 +12,27 @@ import Card from "./Card";
 
 function Skills() {
   return (
-    <Card>
-      <div className="skills">
-        <motion.h2
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-        >
-          Skills
-        </motion.h2>
-        <motion.aside
-          className="icons"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-        >
-          <FontAwesomeIcon icon={faPython} />
-          <FontAwesomeIcon icon={faReact} />
-          <FontAwesomeIcon icon={faGit} />
-          <FontAwesomeIcon icon={faGithub} />
-          <FontAwesomeIcon icon={faJs} />
-        </motion.aside>
-      </div>
-    </Card>
+    <div className="skills">
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+      >
+        Skills
+      </motion.h2>
+      <motion.aside
+        className="icons"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+      >
+        <FontAwesomeIcon icon={faPython} />
+        <FontAwesomeIcon icon={faReact} />
+        <FontAwesomeIcon icon={faGit} />
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faJs} />
+      </motion.aside>
+    </div>
   );
 }
 

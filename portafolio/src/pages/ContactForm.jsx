7fb75f1618b3ac67d47304cefbdb4a36f104f297
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-12 02:00:37 -0500

    form and add react-hook-form

--- portafolio/src/pages/ContactForm.jsx
+++ portafolio/src/pages/ContactForm.jsx
@@ -1,13 +1,18 @@
+import { useForm } from "react-hook-form";
+
 export function ContactForm() {
+  const { register } = useForm();
   return (
     <div>
       <form>
-        <input type="text" placeholder="example@gmail.com">
-          Correo
-        </input>
-        <textarea placeholder="Comentario..." rows="4">
-          Comentario
-        </textarea>
+        <input
+          type="text"
+          placeholder="example@gmail.com"
+          {...register("email", { required: "true" })}
+        />
+        <textarea placeholder="Comentario..." rows="4" />
+        {...register("comment", { required: "true" })}
+        <button>Enviar</button>
       </form>
     </div>
   );

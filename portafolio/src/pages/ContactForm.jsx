3f738fd93d826dd695ebb2958d677f44758beb01
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-14 22:12:10 -0500

    add input
    add new input for email affair

--- portafolio/src/pages/ContactForm.jsx
+++ portafolio/src/pages/ContactForm.jsx
@@ -1,22 +1,30 @@
 import { useForm } from "react-hook-form";
 
 export function ContactForm() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           placeholder="example@gmail.com"
           {...register("email", { required: "true" })}
         />
+        <input
+          type="text"
+          placeholder="Asunto"
+          {...register('subject,{required:"true"}')}
+        ></input>
         <textarea
           placeholder="Comentario..."
           rows="4"
           {...register("comment", { required: "true" })}
         />
 
-        <button>Enviar</button>
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );

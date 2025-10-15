68c05223983185547360693af0388b559fc89950
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-15 00:16:13 -0500

    className

--- portafolio/src/pages/ContactForm.jsx
+++ portafolio/src/pages/ContactForm.jsx
@@ -11,7 +11,7 @@ export function ContactForm() {
     await sendDataforEmail(data);
   };
   return (
-    <div className="flex flex-col flex-wrap gap-3">
+    <div className="div-form">
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"

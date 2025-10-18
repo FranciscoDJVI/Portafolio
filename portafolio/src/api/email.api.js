6ef5295971287b975c369d407e3860108eae8534
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-18 17:04:11 -0500

    change baseURL

--- portafolio/src/api/email.api.js
+++ portafolio/src/api/email.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const emailApi = axios.create({
-  baseURL: "http://localhost:8000/portfolio/api/v1/api_portfilio/",
+  baseURL: "http://localhost:8000/api_portfilio/api/v1/api_portfilio/",
 });
 
 export function sendDataforEmail(data) {

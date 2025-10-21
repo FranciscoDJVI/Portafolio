33e8648ddb23c0d587b2446c2caaa6ab14141d0b
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-21 18:47:10 -0500

    config vite.config

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,29 +1,33 @@
 import "./App.css";
-import Avatar from "./components/Avatar";
-import PresentationText from "./components/PresentationText";
-import Hobbies from "./components/Hobbies";
-import Estudies from "./components/Estudies.jsx";
-import Skills from "./components/Skills";
-import ProjectPersonal from "./components/Projects";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import { ContactForm } from "./pages/ContactForm";
 import { Toaster } from "react-hot-toast";
+import { lazy, Suspense } from "react";
+import { ContactForm } from "./pages/ContactForm";
+
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Avatar = lazy(() => import("./components/Avatar"));
+const PresentationText = lazy(() => import("./components/PresentationText"));
+const Hobbies = lazy(() => import("./components/Hobbies"));
+const Estudies = lazy(() => import("./components/Estudies.jsx"));
+const Skills = lazy(() => import("./components/Skills"));
+const ProjectPersonal = lazy(() => import("./components/Projects"));
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/avatar" element={<Avatar />} />
-        <Route path="/presentation" element={<PresentationText />} />
-        <Route path="/hobbies" element={<Hobbies />} />
-        <Route path="/estudies" element={<Estudies />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<ProjectPersonal />} />
-        <Route path="/contacts" element={<ContactForm />}></Route>
-      </Routes>
-      <Toaster />
+      <suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/avatar" element={<Avatar />} />
+          <Route path="/presentation" element={<PresentationText />} />
+          <Route path="/hobbies" element={<Hobbies />} />
+          <Route path="/estudies" element={<Estudies />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<ProjectPersonal />} />
+          <Route path="/contacts" element={<ContactForm />}></Route>
+        </Routes>
+        <Toaster />
+      </suspense>
     </div>
   );
 }

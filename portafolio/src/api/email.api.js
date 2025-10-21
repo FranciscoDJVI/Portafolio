5963c58a62b6c6ccac0b5e7cb4ba8c8f3f4bc463
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-20 20:18:57 -0500

    reset baseURL

--- portafolio/src/api/email.api.js
+++ portafolio/src/api/email.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const emailApi = axios.create({
   baseURL:
-    "https://backend-porfolio-bhf2.onrender.vercel.app/api_portfolio/api/v1/api_portfilio/",
+    "https://backend-porfolio-bhf2.onrender.com/api_portfilio/api/v1/api_portfilio/",
 });
 
 export function sendDataforEmail(data) {

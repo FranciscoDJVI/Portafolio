c45101a8a6f4b99e3d2a4ff395429f2c59c3b0f9
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-21 19:03:06 -0500

    correct syntax

--- portafolio/vite.config.js
+++ portafolio/vite.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("react") || id.includes("react-dom")) {
-              return "vendor-react";
-            }
-            return "vendor-libs";
+            return "vendor";
           }
         },
       },
